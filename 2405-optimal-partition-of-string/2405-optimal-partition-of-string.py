1ed55571f7c86989b2c60f46790515ab9f439c6c
Rediet <129177195+redietamare@users.noreply.github.com>
2023-06-05 14:01:25 +0300

    Time: 125 ms (62.07%), Space: 17.2 MB (40.06%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,14 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        unique=""
-        string=[]
-        left=0
-        while left<len(s)-1:
-            unique+=s[left]
-            if s[left+1] in unique:
-                string.append(unique)
-                unique=""
-            left+=1
-        string.append(s[left-1:])
-        return(len(string))
+        currentset=set()
+        minnum=1
+        for i in s:
+            if i in currentset:
+                minnum+=1
+                currentset=set()
+            currentset.add(i)
+        return(minnum)
 
