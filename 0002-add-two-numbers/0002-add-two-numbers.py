491b0c93c77c379d5515cdb7b544f9094508aa42
Rediet <129177195+redietamare@users.noreply.github.com>
2023-06-12 14:25:41 +0300

    Time: 72 ms (78.94%), Space: 16.3 MB (75.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,41 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1=l1
-        curr2=l2
-        prev1,prev2=None,None
-        while curr1:
-            nextcurr1=curr1.next
-            curr1.next=prev1
-            prev1=curr1
-            curr1=nextcurr1
-        while curr2:
-            nextcurr2=curr2.next
-            curr2.next=prev2
-            prev2=curr2
-            curr2=nextcurr2
-        list1=""
-        list2=""
-        while prev1:
-            list1+=str(prev1.val)
-            prev1=prev1.next
-        while prev2:
-            list2+=str(prev2.val)
-            prev2=prev2.next
-        intlist1=int(list1)
-        intlist2=int(list2)
-        summ=str(intlist1+intlist2)
-        sumlist=list(summ)
-        sumlist.reverse()
-        dummy1=ListNode(0)
-        sumhead=dummy1
-        dummy2=ListNode(None)
-        for i in range(len(sumlist)):
-            dummy2.val=int(sumlist[i])
-            dummy1.next=dummy2
-            dummy1=dummy1.next
-            dummy2=ListNode(None)
-        return(sumhead.next)
-        
-        
-            
+        dummy=ListNode()
+        curr=dummy
+        carry=0
+        while l1 or l2 or carry:
+            if l1: val1=l1.val
+            else: val1=0
+            if l2: val2=l2.val
+            else: val2=0
+            value=val1+val2+carry
+            carry=value//10
+            value=value%10
+            curr.next=ListNode(value)
+            curr=curr.next
+            if l1: l1=l1.next
+            else:l1=None
+            if l2:l2=l2.next
+            else: None
+        return(dummy.next)
