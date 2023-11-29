d55ae93822fb053120a2bca43956badd0744caa3
Rediet <129177195+redietamare@users.noreply.github.com>
2023-11-29 18:17:34 +0300

    Added solution for Palindrome Number

--- leet-code-solutions/palindrome-number.py
+++ leet-code-solutions/palindrome-number.py
@@ -3,30 +3,3 @@ class Solution:
         num=str(x)
         numm=num[::-1]
         return numm==num
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # s=str(x)
-        # revstr=""
-        # for i in range(len(s)):
-        #     revstr+=s[len(s)-i-1]
-        # if revstr==s:return True
-        # else: return False
