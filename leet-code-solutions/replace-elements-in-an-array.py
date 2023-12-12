ac303283e6f5af75b0bcff3e59083e61c9599197
Rediet <129177195+redietamare@users.noreply.github.com>
2023-12-12 17:34:27 +0300

    Added solution for Replace Elements in an Array

--- leet-code-solutions/replace-elements-in-an-array.py
+++ leet-code-solutions/replace-elements-in-an-array.py
@@ -1,13 +1,42 @@
 class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
-        hashtable={}
+        hashmap={}
         for i in range(len(nums)):
-            hashtable[nums[i]]=i
-        for i in range(len(operations)):
-            hashtable[operations[i][1]]=hashtable[operations[i][0]]
-            hashtable[operations[i][0]]=None
+            hashmap[nums[i]]=i
+        
+        for ops in operations:
+            hashmap[ops[1]]=hashmap[ops[0]]
+            del hashmap[ops[0]]
+        
+        for i in hashmap:
+            if hashmap[i]!=None:
+                nums[hashmap[i]]=i
+        return nums
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+#         hashtable={}
+#         for i in range(len(nums)):
+#             hashtable[nums[i]]=i
+#         print(hashtable)
+#         for i in range(len(operations)):
+#             hashtable[operations[i][1]]=hashtable[operations[i][0]]
+#             hashtable[operations[i][0]]=None
+#             print(hashtable)
+        
 
-        for i in hashtable:
-            if hashtable[i]!=None:
-                nums[hashtable[i]]=i
-        return(nums)
+#         for i in hashtable:
+#             if hashtable[i]!=None:
+#                 nums[hashtable[i]]=i
+#         return(nums)
