1c2dfc86c5efdd4e941429f27e28ec11f711caf6
Rediet <129177195+redietamare@users.noreply.github.com>
2023-12-29 17:24:56 +0300

    Added solution for Sorting the Sentence

---
+++ leet-code-solutions/sorting-the-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s=s.split()
+        s.sort(key=lambda x:x[-1])
+
+        for i in range(len(s)):
+            s[i]=s[i][:-1]
+        return " ".join(s)
