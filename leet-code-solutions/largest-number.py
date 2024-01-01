c82b2826a44344884b97e16463d31c0e0b1af9d7
Rediet <129177195+redietamare@users.noreply.github.com>
2024-01-01 13:03:20 +0300

    Added solution for Largest Number

---
+++ leet-code-solutions/largest-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            nums[i]=str(nums[i])
+        def compare(n1,n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+        nums=sorted(nums,key=cmp_to_key(compare))
+        return str(int("".join(nums)))
